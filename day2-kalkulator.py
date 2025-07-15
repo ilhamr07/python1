f1a65a926268e544114dfce06f6f68a9648a6f19
Mahli <data.ilhamr@gmail.com>
2025-07-15 15:18:59 +0700

    update loop kalkulator

--- day2-kalkulator.py
+++ day2-kalkulator.py
@@ -1,17 +1,26 @@
-angka_awal = int(input("Masukan angka pertama : "))
-angka_kedua = int(input("Masukan angka kedua : "))
-operator = input("Masukan Operator (+ , - , * , / ):")
 
+while True: #loop dengan kondisi selalu benarr
+    angka_awal = int(input("Masukan angka pertama : "))
+    angka_kedua = int(input("Masukan angka kedua : "))
+    operator = input("Masukan Operator (+ , - , * , / ):")
 
-if operator == "+":
-    hitungan = angka_awal + angka_kedua
-elif operator =="-":
-    hitungan = angka_awal - angka_kedua
-elif operator =="*":
-    hitungan = angka_awal * angka_kedua
-elif operator =="/":
-    hitungan = angka_awal / angka_kedua
-else :
-    print("operator matematika yang ada masukan tidak ada !!")
+    if operator == "+":
+        hitungan = angka_awal + angka_kedua
+    elif operator =="-":
+        hitungan = angka_awal - angka_kedua
+    elif operator =="*":
+        hitungan = angka_awal * angka_kedua
+    elif operator =="/":
+        hitungan = angka_awal / angka_kedua
+    else :
+        print("operator matematika yang ada masukan tidak ada !!")
 
-print(f"Hasil :{hitungan}")
+    print(f"Hasil :{hitungan}")
+    loop= input(f"Hitung Lagi? (y/n): ")
+    if loop =="n":
+        print("Permainan Berakhirrr")
+        break #keluar dari perulangan karna pemain ingin mengakhiri game
+    else :
+        print("Lanjut Berhitunggg!")
+    
+
