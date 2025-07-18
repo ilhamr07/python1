66f2b36d7a4984908e6ef125a44d97ea4887de3c
Mahli <data.ilhamr@gmail.com>
2025-07-18 23:59:23 +0700

    update method serang

--- day6_oop_dasar.py
+++ day6_oop_dasar.py
@@ -4,7 +4,13 @@ class Hero:
         self.health=health
         self.power=power
         self.defend= defend
+    
+    def serang(self):
+        print(self.name,"Menyerang")
+
 
 hero1 = Hero("badang",1400,200,80)
+
+hero1.serang()
 
 print(hero1.__dict__)
