97369b861168e4fb9996445d27daeab0acd24210
Mahli <data.ilhamr@gmail.com>
2025-07-19 00:46:50 +0700

    update oop

--- day6_oop_dasar.py
+++ day6_oop_dasar.py
@@ -5,12 +5,27 @@ class Hero:
         self.power=power
         self.defend= defend
     
-    def serang(self):
-        print(self.name,"Menyerang")
+    def serang(self, lawan):
+        print(self.name,"Menyerang", lawan.name, "dengan attack =",self.power)
+        lawan.diserang(self)
+
+    def diserang(self,lawan):
+        darah_diserang= self.health-lawan.power
+        print(f"Health",self.name ," saat ini : ",darah_diserang)
+        print(self.name," menyerang balik ",lawan.name,"dengan attack:",self.power )
+        darah_penyerang = lawan.health - self.power
+        print(f"health {lawan.name} saat ini :{darah_penyerang}")
 
 
 hero1 = Hero("badang",1400,200,80)
+hero2 = Hero("Lapu-Lapu",1400,250,10)
 
-hero1.serang()
+hero1.serang(hero2)
+print()
+hero1.serang(hero2)
+print()
+hero2.serang(hero1)
+print()
 
 print(hero1.__dict__)
+print(hero2.__dict__)
