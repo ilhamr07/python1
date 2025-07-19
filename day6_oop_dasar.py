37d1d11f49dc779d689809d9094cffaa4520c2ff
Mahli <data.ilhamr@gmail.com>
2025-07-19 17:12:40 +0700

    perbaikan logika fungsi

--- day6_oop_dasar.py
+++ day6_oop_dasar.py
@@ -10,22 +10,27 @@ class Hero:
         lawan.diserang(self)
 
     def diserang(self,lawan):
-        darah_diserang= self.health-lawan.power
-        print(f"Health",self.name ," saat ini : ",darah_diserang)
+        self.health = self.health-lawan.power
+        print(f"Health",self.name ," saat ini : ",self.health)
         print(self.name," menyerang balik ",lawan.name,"dengan attack:",self.power )
-        darah_penyerang = lawan.health - self.power
-        print(f"health {lawan.name} saat ini :{darah_penyerang}")
+        lawan.health = lawan.health - self.power
+        print(f"health {lawan.name} saat ini :{lawan.health}")
 
 
 hero1 = Hero("badang",1400,200,80)
 hero2 = Hero("Lapu-Lapu",1400,250,10)
 
 hero1.serang(hero2)
-print()
-hero1.serang(hero2)
-print()
-hero2.serang(hero1)
-print()
-
 print(hero1.__dict__)
 print(hero2.__dict__)
+print()
+
+hero1.serang(hero2)
+print(hero1.__dict__)
+print(hero2.__dict__)
+print()
+
+hero2.serang(hero1)
+print(hero1.__dict__)
+print(hero2.__dict__)
+print()
