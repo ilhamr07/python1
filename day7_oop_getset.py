9166be2f2da67a37c35cb669e37e9343289a3439
Mahli <data.ilhamr@gmail.com>
2025-07-20 23:46:11 +0700

    update oop

--- day7_oop_getset.py
+++ day7_oop_getset.py
@@ -5,10 +5,7 @@ class Hero:
         self.__health = health
         self.__attackPower = attackPower
         self.__defend = defend
-    
-    @name.getter
-    def name(self):
-        return self.__name
+
     
     @property
     def info(self):
@@ -28,17 +25,24 @@ class Hero:
         lawan.serangBalik(self)
 
     def serangBalik(self,lawan):
-        print(f"{self.__name} menyerang {lawan.__name} dengan attack : {self.__attackPower}")
+        print(f"{self.name} menyerang {lawan.name} dengan attack : {self.__attackPower}")
         lawan.__health = lawan.__health - self.__attackPower 
         print(f"darah lapu-lapu saat ini : {lawan.__health}")
         print()
+    
+    @property
+    def name(self):
+        pass
+
+    @name.getter
+    def name(self):
+        return self.__name
 
    
 
 lapuLapu = Hero("lapuLapu",100,10,20)
 vexana = Hero("vexana",80,30,15)
 
-lapuLapu.name="Ujang"
 print(lapuLapu.info)
 print(vexana.info)
 
