b570ebba0ed110bc6ec3ccd85ef8479af76a8fa3
Mahli <data.ilhamr@gmail.com>
2025-07-20 23:02:33 +0700

    update

--- day7_oop_getset.py
+++ day7_oop_getset.py
@@ -1,18 +1,23 @@
 class Hero:
 
-    def __init__(self, __name, health, attackPower, defend):
-        self.__name = __name
+    def __init__(self, name, health, attackPower, defend):
+        self.__name = name
         self.__health = health
         self.__attackPower = attackPower
         self.__defend = defend
-        
+    
+    @name.getter
+    def name(self):
+        return self.__name
     
     @property
     def info(self):
         return "nickname :{} \n" \
         "\thealth :{} \n" \
         "\tattackPower :{} \n" \
-        "\tDefend :{}"    .format(self.__name,self.__health,self.__attackPower,self.__defend)
+        "\tDefend :{}".format(self.__name,self.__health,self.__attackPower,self.__defend)
+
+
 
     def attack(self, lawan):
         print(f"{self.__name} melakukan penyerangan ke {lawan.__name} dengan attack : {self.__attackPower}")
@@ -27,16 +32,20 @@ class Hero:
         lawan.__health = lawan.__health - self.__attackPower 
         print(f"darah lapu-lapu saat ini : {lawan.__health}")
         print()
-        
+
+   
+
 lapuLapu = Hero("lapuLapu",100,10,20)
 vexana = Hero("vexana",80,30,15)
 
+lapuLapu.name="Ujang"
 print(lapuLapu.info)
 print(vexana.info)
 
 lapuLapu.attack(vexana)
 lapuLapu.attack(vexana)
 vexana.attack(lapuLapu)
+
 
 print(lapuLapu.info)
 print(vexana.info)
